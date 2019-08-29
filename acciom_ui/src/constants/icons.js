db24e83e601b59718dc51613ea813f949005aad0
Harshad Kavathiya <harshad3011@gmail.com>
2019-08-29 17:08:25 +0530

    Removed React old code base

--- acciom_ui/src/constants/icons.js
+++
@@ -1,4 +0,0 @@
-export const ICON_STATUS_SUCCESS = 'fas fa-check-circle statusCheckIcon';
-export const ICON_STATUS_FAIL = 'fas fa-times-circle statusDelIcon';
-export const ICON_STATUS_ERROR = 'fas fa-exclamation-circle statusDelIcon';
-export const ICON_STATUS_INPROGRESS = 'fas fa-hourglass-half statusInProgressIcon';
