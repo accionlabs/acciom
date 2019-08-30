5243cca68bfc74772d48ac3217675813eb7e45c7
Akhil Bhardwaj <bhardwaj.akhil96@gmail.com>
2019-08-30 15:55:32 +0530

    Organization Api

--- application/api/organization.py
+++ application/api/organization.py
@@ -2,7 +2,6 @@
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
-
 from flask_restful import Resource, reqparse
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -30,24 +29,22 @@ class OrganizationAPI(Resource):
         Returns: Standard API Response with HTTP status code
 
         """
+        user_id = session.user_id
+        user_obj = User.query.filter_by(user_id=user_id,
+                                        is_deleted=False).first()
         create_org_parser = reqparse.RequestParser(bundle_errors=True)
         create_org_parser.add_argument(
             'org_name', help=APIMessages.PARSER_MESSAGE,
             required=True, type=str)
         create_org_data = create_org_parser.parse_args()
-        try:
-            create_organization = Organization(create_org_data['org_name'],
-                                               session.user_id)
-            create_organization.save_to_db()
-            organization_data = {'org_id': create_organization.org_id,
-                                 'org_name': create_organization.org_name}
-            return api_response(
-                True, APIMessages.CREATE_RESOURCE.format('Organization'),
-                STATUS_CREATED, organization_data)
-        except Exception as e:
-            return api_response(
-                False, APIMessages.INTERNAL_ERROR, STATUS_SERVER_ERROR,
-                {'error_log': str(e)})
+        create_organization = Organization(create_org_data['org_name'],
+                                           session.user_id)
+        create_organization.save_to_db()
+        organization_data = {'org_id': create_organization.org_id,
+                             'org_name': create_organization.org_name}
+        return api_response(
+            True, APIMessages.CREATE_RESOURCE.format('Organization'),
+            STATUS_CREATED, organization_data)
 
     @token_required
     def put(self, session):
@@ -68,18 +65,13 @@ class OrganizationAPI(Resource):
             'org_name', help=APIMessages.PARSER_MESSAGE,
             required=True, type=str)
         update_org_data = update_org_parser.parse_args()
-        try:
-            current_org = Organization.query.filter_by(
-                org_id=update_org_data['org_id']).first()
-            current_org.org_name = update_org_data['org_name']
-            current_org.save_to_db()
-            return api_response(
-                True, APIMessages.UPDATE_RESOURCE.format('Organization'),
-                STATUS_OK)
-        except Exception as e:
-            return api_response(
-                False, APIMessages.INTERNAL_ERROR, STATUS_SERVER_ERROR,
-                {'error_log': str(e)})
+        current_org = Organization.query.filter_by(
+            org_id=update_org_data['org_id']).first()
+        current_org.org_name = update_org_data['org_name']
+        current_org.save_to_db()
+        return api_response(
+            True, APIMessages.UPDATE_RESOURCE.format('Organization'),
+            STATUS_OK)
 
     @token_required
     def get(self, session):
@@ -91,30 +83,25 @@ class OrganizationAPI(Resource):
 
         Returns: Standard API Response with HTTP status code
         """
-        try:
-            # TODO: Currently, get call will give all
-            #  organizations which are active
-            # TODO: Implement a logic to return organizations that user is part
-            # Storing all active projects in a list
-            list_of_active_orgs = Organization.query.filter_by(
-                is_deleted=False).all()
-            if not list_of_active_orgs:
-                return api_response(
-                    False, APIMessages.NO_RESOURCE.format('Organization'),
-                    STATUS_OK, STATUS_UNAUTHORIZED)
-            # list of projects to be returned in the response
-            org_details_to_return = list()
-            for each_project in list_of_active_orgs:
-                org_details_to_return.append(
-                    {'org_id': each_project.org_id,
-                     'org_name': each_project.org_name})
+        # TODO: Currently, get call will give all
+        #  organizations which are active
+        # TODO: Implement a logic to return organizations that user is part
+        # Storing all active projects in a list
+        list_of_active_orgs = Organization.query.filter_by(
+            is_deleted=False).all()
+        if not list_of_active_orgs:
             return api_response(
-                True, APIMessages.SUCCESS, STATUS_OK,
-                {"organization_details": org_details_to_return})
-        except Exception as e:
-            return api_response(
-                False, APIMessages.INTERNAL_ERROR, STATUS_SERVER_ERROR,
-                {'error_log': str(e)})
+                False, APIMessages.NO_RESOURCE.format('Organization'),
+                STATUS_OK, STATUS_UNAUTHORIZED)
+        # list of projects to be returned in the response
+        org_details_to_return = list()
+        for each_project in list_of_active_orgs:
+            org_details_to_return.append(
+                {'org_id': each_project.org_id,
+                 'org_name': each_project.org_name})
+        return api_response(
+            True, APIMessages.SUCCESS, STATUS_OK,
+            {"organization_details": org_details_to_return})
 
 
 class DashBoardStatus(Resource):
