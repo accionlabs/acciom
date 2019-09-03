0ea60a923db735dcbc93554c8e2eb149b815bc78
Gautam Nagi <Gautam Nagi@accionlabs.in>
2019-09-03 17:39:34 +0530

    Resolved state onclick of View in Test Cases Row

--- acciom_ui/src/containers/TestSuiteList.js
+++ acciom_ui/src/containers/TestSuiteList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-
+	
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
