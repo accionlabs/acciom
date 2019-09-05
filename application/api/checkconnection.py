c9aa6d7d3b100eeb9f4e2693727ce005ba786579
Roja T <rojatgud@gmail.com>
2019-09-05 16:05:51 +0530

    change in checking spaces in hostname or username

--- application/api/checkconnection.py
+++ application/api/checkconnection.py
@@ -60,9 +60,7 @@ class CheckConnection(Resource):
                                 http_status_code=STATUS_BAD_REQUEST,
                                 data={})
         # Checking spaces in username and hostname
-        spacecount_dbusername = db_data["db_username"].find(" ")
-        spacecount_dbhostanme = db_data["db_hostname"].find(" ")
-        if spacecount_dbusername > -1 or spacecount_dbhostanme > -1:
+        if " " in db_data["db_username"] or " " in db_data["db_hostname"]:
             return api_response(False, APIMessages.
                                 NO_SPACES,
                                 STATUS_BAD_REQUEST)
