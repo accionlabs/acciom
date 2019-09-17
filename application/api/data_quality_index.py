ffa0d407cb84fb5920371e463cea0699ae896d0c
Harshad Kavathiya <harshad3011@gmail.com>
2019-09-17 17:26:33 +0530

    Added Start & End dates in DQI History API

--- application/api/data_quality_index.py
+++ application/api/data_quality_index.py
@@ -235,17 +235,18 @@ class ProjectDQIHistory(Resource):
         except ValueError:
             raise GenericBadRequestException(APIMessages.DATE_FORMAT)
 
-        if not start_date and not end_date:
-            # If start and end date are not given, take current month range
-            current_day = dt.today()
-            current_month_first_day = date.today().replace(day=1)
-            start_date = current_month_first_day
-            end_date = current_day
-
         # calling get_project_dqi_history to get day wise data
         daily_dqi = get_project_dqi_history(
             dqi_history_data['project_id'], start_date=start_date,
             end_date=end_date)
+
+        if not start_date and not end_date:
+            # If start and end date are not given, take current month range
+            current_day = dt.today()
+            current_month_first_day = date.today().replace(day=1)
+            start_date = current_month_first_day.strftime("%Y-%m-%d")
+            end_date = current_day.strftime("%Y-%m-%d")
+            
         dqi_response = OrderedDict()
         dqi_response['project_id'] = dqi_history_data['project_id']
         dqi_response['project_name'] = check_valid_project.project_name
